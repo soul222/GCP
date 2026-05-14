d411f533fbc7b968568cdbc1e5b6c4ef54bbd93f
soul222 <soultanamirul222@gmail.com>
2026-05-14 22:37:18 +0700

    Fix permissions with native git executable flag

--- .platform/hooks/postdeploy/01_permissions.sh
+++ .platform/hooks/postdeploy/01_permissions.sh
@@ -3,3 +3,4 @@ chmod -R 775 /var/app/current/storage
 chmod -R 775 /var/app/current/bootstrap/cache
 chown -R webapp:webapp /var/app/current/storage
 chown -R webapp:webapp /var/app/current/bootstrap/cache
+
