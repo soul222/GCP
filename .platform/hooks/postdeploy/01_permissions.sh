d6603a604bc01556bb4f37fe4aed836239db2ced
soul222 <soultanamirul222@gmail.com>
2026-05-14 22:52:12 +0700

    Fix ondeck absolute path caching issue

--- .platform/hooks/postdeploy/01_permissions.sh
+++ .platform/hooks/postdeploy/01_permissions.sh
@@ -3,4 +3,9 @@ chmod -R 775 /var/app/current/storage
 chmod -R 775 /var/app/current/bootstrap/cache
 chown -R webapp:webapp /var/app/current/storage
 chown -R webapp:webapp /var/app/current/bootstrap/cache
+cd /var/app/current
+php artisan optimize:clear
+php artisan view:clear
+php artisan config:clear
+php artisan optimize
 
